8e1254744095965e37ba1895267a4fabbc564b4a
bella-cockrell <heybellac@gmail.com>
2020-03-30 23:36:28 +0100

    Adding render code for UserItem

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: 'id',
-      login: 'mojombo',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo'
-    }
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo'
   }
 
-
   render() {
+    const { avatar_url, login, html_url } = this.state;
+    //destructoring
     return (
-      <div>
-        User Item
+      <div className='card text-center'>
+        <img
+          src={avatar_url}
+          alt={Object.keys(avatar_url)}
+          className='round-img'
+          style={{ width: '60px' }} />
+        <h3>{login}</h3>
+        <div>
+          <a
+            href={html_url}
+            className="btn btn-dark btn-sm my-1">More
+          </a>
+        </div>
       </div>
     )
   }

81da6cd2cae4665d750478a5f1f675ef16a2321a
bella-cockrell <heybellac@gmail.com>
2020-03-30 21:54:13 +0100

    Adding propTypes to Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,21 @@
 //class-based component
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 export class Navbar extends Component {
+  //These will be called if there is no prop passed. Passed props will override this.
   static defaultProps = {
     title: 'Github Finder',
     icon: 'fab fa-github'
   };
-  
+
+  //This checks the types of each prop. make sure to import.
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icont: PropTypes.string.isRequired
+  };
+
   render() {
     return (
       <nav className='navbar bg-primary'>

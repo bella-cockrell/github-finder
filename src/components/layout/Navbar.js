f8f2397b720f9d784e78f55ea2ba6252b90713b5
bella-cockrell <heybellac@gmail.com>
2020-04-02 12:29:50 +0100

    Refactoring Navbar to function

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,31 +1,28 @@
 //class-based component
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types' //This functions same as TypeScript
 
-export class Navbar extends Component {
-  //These will be called if there is no prop passed. Passed props will override this.
-  static defaultProps = {
-    title: 'Github Finder',
-    icon: 'fab fa-github'
-  }
+const Navbar = props => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={props.icon} /> {props.title}
+      </h1>
+    </nav>
+  )
+}
 
-  //This checks the types of each prop. make sure to import.
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  }
+//These will be called if there is no prop passed. Passed props will override this.
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github'
+}
 
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    )
-  }
+//This checks the types of each prop. make sure to import.
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
 }
 
 export default Navbar
-
 //fab fa-github is from Font Awesome

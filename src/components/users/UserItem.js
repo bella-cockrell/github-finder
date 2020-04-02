d188841078919f97ecaeb0420c78ffd467466cce
bella-cockrell <heybellac@gmail.com>
2020-04-02 12:30:17 +0100

    Refactoring UserItem to function

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class UserItem extends Component {
-  render() {
-    const { avatar_url, login, html_url } = this.props.user
-    //destructoring from the props in the mapping method in Users.js
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt={login}
-          className="round-img"
-          style={{ width: '60px' }} //you can pass a style without CSS. Need double curly braces.
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+const UserItem = props => {
+  const { avatar_url, login, html_url } = props.user
+  //destructoring from the props in the mapping method in Users.js
+
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt={login}
+        className="round-img"
+        style={{ width: '60px' }} //you can pass a style without CSS. Need double curly braces.
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default UserItem

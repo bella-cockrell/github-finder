1bb77b204d3b72cfa788e6545ea5c239ee169f15
bella-cockrell <heybellac@gmail.com>
2020-04-03 16:15:54 +0100

    Adding comments to help me remember all this

--- src/App.js
+++ src/App.js
@@ -9,21 +9,22 @@ class App extends Component {
     users: [],
     loading: false
   }
-
+  //Step 1: set the initial state
   async componentDidMount() {
     this.setState({ loading: true })
-
+    //Step 2: change the state
     const res = await axios.get('https://api.github.com/users')
 
-    this.setState({ users: res.data, loading: false }) //.data is axios
+    this.setState({ users: res.data, loading: false })
   }
-
+  //Step 3: execute action when state is changed, revert back to original state.
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
           <Users loading={this.state.loading} users={this.state.users} />
+          {/* Step 4: pass both states as props to be rendered*/}
         </div>
       </div>
     )

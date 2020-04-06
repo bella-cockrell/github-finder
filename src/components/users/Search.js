616a8ecf9ce8237a8c77f82bd2d6e953f13f95d2
bella-cockrell <heybellac@gmail.com>
2020-04-06 16:30:38 +0100

    Adding event handler to state

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 
 export class Search extends Component {
+  state = {
+    text: '',
+  }
+  /*If a stateful component has an event handler, the event needs to be defined
+before the render. Note: e stands for event */
+  onChange = (e) => {
+    this.setState({ text: e.target.value })
+  }
+  //.target refers to the object onto which the event was dispatched: 'text'
+
   render() {
     return (
       <div>
         <form className="form">
-          <input type="text" name="text" placeholder="Search users..." />
+          <input
+            type="text"
+            name="text"
+            placeholder="Search users..."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
           <input
             type="submit"
             value="Search"

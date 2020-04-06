a6559e7b89e95683af8208132a8bc28548c4149a
bella-cockrell <heybellac@gmail.com>
2020-04-06 13:02:16 +0100

    Updating GET with client tokens

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import './App.css'
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
   }
   //Step 1: set the initial state
   async componentDidMount() {
     this.setState({ loading: true })
     //Step 2: change the state
-    const res = await axios.get('https://api.github.com/users')
+    const res = await axios.get(`https://api.github.com/users?client_id
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
 
     this.setState({ users: res.data, loading: false })
   }

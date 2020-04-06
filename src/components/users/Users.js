7cf3dcdf9ee367952bf58e03478646e72f3da0c3
bella-cockrell <heybellac@gmail.com>
2020-04-06 10:48:28 +0100

    Refactoring Users.js from class to function

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
 
-class Users extends Component {
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.props.users.map(user => (
-          <UserItem key={user.id} user={user} />
-          //This mapping method defines a prop which is used in UserItem.js
-        ))}
-      </div>
-    )
-  }
+const Users = ({ users, loading }) => {
+  return (
+    <div style={userStyle}>
+      {users.map((user) => (
+        <UserItem key={user.id} user={user} />
+        //This mapping method defines a prop which is used in UserItem.js
+      ))}
+    </div>
+  )
 }
 
 const userStyle = {
   display: 'grid', //If not in CSS, must put value in apos
   gridTemplateColumns: 'repeat(3, 1fr)', //If not in CSS, must use pascalcase not kebab
-  gridGap: '1rem'
+  gridGap: '1rem',
 }
 export default Users

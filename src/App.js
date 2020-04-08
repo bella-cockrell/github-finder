2b3bae235cd47df5cf71f34d14b7912d54c54810
bella-cockrell <heybellac@gmail.com>
2020-04-08 17:18:25 +0100

    Cleaning up comments

--- src/App.js
+++ src/App.js
@@ -8,27 +8,32 @@ import './App.css'
 class App extends Component {
   state = {
     users: [],
-    loading: false,
+    loading: false //1
   }
-  //Step 1: set the initial state
+
   async componentDidMount() {
-    this.setState({ loading: true })
-    //Step 2: change the state
+    this.setState({ loading: true }); //2
+
     const res = await axios.get(`https://api.github.com/users?client_id
       ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret
-      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`); //3
 
-    this.setState({ users: res.data, loading: false })
+    this.setState({ users: res.data, loading: false }) //4
   }
-  //Step 3: execute action when state is changed, revert back to original state.
+
+  //Search Github users
+  searchUsers = (text) => {
+    console.log(text)
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
-          {/* Step 4: pass both states as props to be rendered*/}
+          {/*5*/}
         </div>
       </div>
     )
@@ -36,3 +41,12 @@ class App extends Component {
 }
 
 export default App
+
+/*
+HOW TO PASS STATE CHANGE
+Step 1: Set the initial state in state object.
+Step 2: Change the state through this.setState. This change can be through an API call or event.
+Step 3: Execute action when state is changed.
+Step 4: Revert back to original state once action is complete.
+Step 5: Pass both states as props to be rendered, either true or false (and subsequent condition).
+*/
